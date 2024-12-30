04854efad5b05ac7564eba9ce6a0630bb9a7c6f8
Choi-Shin <shincho1990@naver.com>
2024-12-30 17:45:26 +0900

    next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: ["lh3.googleusercontent.com"],
+    remotePatterns: [
+      {protocol: "https", hostname: "lh3.googleusercontent.com"}
+    ]
   },
 };
 
