e9e3b49e7c1e5ff10f285bf4549683bc9e1807d3
Choi-Shin <shincho1990@naver.com>
2025-02-12 22:02:32 +0900

    typing

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 "use client";
+
+import MultiTypingEffect from "./util/MultiTypingEffect";
+
 // import { useSearchParams } from "next/navigation";
 // import { useEffect } from "react";
 
 export default function Home() {
-  return <>왐마</>;
+  const texts = ["안녕하시오?", "반가워용"];
+  return (
+    <>
+      <MultiTypingEffect texts={texts}></MultiTypingEffect>
+    </>
+  );
 }
